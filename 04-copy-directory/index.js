1a97c9d571faa9b2cb395265125b41b20e13a7aa
Svetlana Safonova <safonova.sv.a@gmail.com>
2022-11-02 17:13:54 +0300

    feat: solve task files in folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const mkdir = fs.promises.mkdir;
+const { copyFile, constants } = fs.promises;
 
+async function copyDir () {
+  const dirName =  path.join(__dirname, 'files');
+  const dirNameCopy = path.join(__dirname, 'filesCopy');
+  const createDir = await mkdir(dirNameCopy, { recursive: true });
+}
+
+copyDir();
