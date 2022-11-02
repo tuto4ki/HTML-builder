40f2eeea82cac81c8e87dc29d46888515bc72aab
Svetlana Safonova <safonova.sv.a@gmail.com>
2022-11-02 17:40:02 +0300

    feat: add function copyDir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,12 +1,27 @@
 const fs = require('fs');
 const path = require('path');
-const mkdir = fs.promises.mkdir;
-const { copyFile, constants } = fs.promises;
 
 async function copyDir () {
-  const dirName =  path.join(__dirname, 'files');
-  const dirNameCopy = path.join(__dirname, 'filesCopy');
-  const createDir = await mkdir(dirNameCopy, { recursive: true });
+    const mkdir = fs.promises.mkdir;
+    const copyFile = fs.promises.copyFile;
+    const deleteFile = fs.unlink;
+    const dirName =  path.join(__dirname, 'files');
+    const dirNameCopy = path.join(__dirname, 'filesCopy');
+    const createDir = await mkdir(dirNameCopy, { recursive: true });
+    fs.readdir(dirNameCopy,
+        (error, items) => {
+            for (let i = 0; i < items.length; i++) {
+                deleteFile(path.join(dirNameCopy, items[i]), error => {if(error) throw error; ''});
+            }
+    });
+          
+    fs.readdir(dirName,
+        (error, items) => {
+            for (let i = 0; i < items.length; i++) {
+                copyFile(path.join(dirName, items[i]), path.join(dirNameCopy, items[i]));
+            }
+        }
+    );
 }
 
 copyDir();
